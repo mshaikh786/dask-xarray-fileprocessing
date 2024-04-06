d2f2d23a67afb966c3d09ca916ba8c2785177a90
mshaikh786 <mohsinshaikh786@gmail.com>
2024-04-06 23:04:18 +0300

    making scripts generic

--- dask_futures_xarray.py
+++ dask_futures_xarray.py
@@ -3,15 +3,14 @@ import dask
 from dask.distributed import Client,as_completed
 import xarray
 
-
-client=Client(scheduler_file='../scheduler.json')
+job_id=os.environ['SLURM_JOBID']
+client=Client(scheduler_file='scheduler_%s.json'%job_id)
 print(client)
 
 
-datasets=xarray.open_mfdataset(
-    '/scratch/shaima0d/user_codes/*.nc',
-engine='netcdf4',
-chunks={'time':50})
+datasets=xarray.open_mfdataset('%s/*.nc'%(os.environ['DATA_DIR']),
+                               engine='netcdf4',
+                               chunks={'time':50})
 
 
 with dask.config.set(**{'array.slicing.split_large_chunks': False}):
@@ -22,12 +21,12 @@ print(ytmp)
 
 
 
-os.system('rm -rf /scratch/shaima0d/results/*')
+os.system('rm -rf %s/*'%(os.environ['RESULTS_DIR'])
 
 
 paths=list()
 for y in range(len(dtmp)):
-    paths.append('/scratch/shaima0d/results/tmp_%d.nc'%ytmp[y])
+    paths.append('%s/tmp_%d.nc'%(os.environ['RESULTS_DIR'],ytmp[y]))
 
 
 
